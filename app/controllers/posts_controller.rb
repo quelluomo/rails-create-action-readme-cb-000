82b691bf93db48dd4d5a07f24dbea1add132cd13
Matteo Muscas <thatman.j@gmail.com>
2019-05-22 09:39:13 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-    def create
+  def create
     post = Post.new
     post.title = params[:title]
     post.description = params[:description]
